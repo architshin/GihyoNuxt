10cf016809e1c42802e6c488cc6e334522b8fd43
Shinzo SAITO <architshin@websarva.com>
2022-12-16 18:12:19 +0900

    routing-basicを追加。

---
+++ chap03/routing-basic/pages/index.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+</script>
+
+<template>
+	<h1>TOP</h1>
+	<nav id="breadcrumbs">
+		<ul>
+			<li>TOP</li>
+		</ul>
+	</nav>
+	<section>
+		<p>
+			<NuxtLink to="/member/memberList">
+				会員管理はこちら
+			</NuxtLink>
+		</p>
+	</section>
+</template>
