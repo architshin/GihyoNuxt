746a7d976edc47ff4aaa35cb6fc129807f237ebc
Shinzo SAITO <architshin@websarva.com>
2023-01-20 16:51:19 +0900

    lazyasyncdataサンプルを追加。

---
+++ chap04/lazyasyncdata/pages/WeatherInfo/[id].vue
@@ -0,0 +1,67 @@
+<script setup lang="ts">
+import type {City} from "@/interfaces";
+
+const route = useRoute();
+const cityList = useState<Map<number, City>>("cityList");
+const selectedCity = computed(
+	(): City => {
+		const idNo = Number(route.params.id);
+		return cityList.value.get(idNo) as City;
+	}
+);
+const weatherDescription = ref("");
+
+const asyncData = useLazyAsyncData(
+	`/WeatherInfo/${route.params.id}`,
+	async () => {
+		//アクセス先URLの基本部分の変数を用意。
+		const weatherInfoUrl = "http://api.openweathermap.org/data/2.5/weather";
+		//クエリパラメータの元データとなるオブジェクトリテラルを用意。
+		const params:{
+			lang: string,
+			q: string,
+			appId: string
+		} =
+		{
+			//言語設定のクエリパラメータ
+			lang: "ja",
+			//都市を表すクエリパラメータ。
+			q: selectedCity.value.q,
+			//APIキーのクエリパラメータ。ここに各自の文字列を記述する!!
+			appId: "913136635cfa3182bbe18e34ffd44849"
+		}
+		//クエリパラメータを生成。
+		const queryParams = new URLSearchParams(params);
+		//実際にアクセスするURLを生成。
+		const urlFull = `${weatherInfoUrl}?${queryParams}`;
+		//URLに非同期でアクセスしてデータを取得。
+		const response = await $fetch(urlFull) as any;
+		return response;
+	},
+	{
+		transform: (data): string => {
+			const weatherArray = data.weather;
+			const weather = weatherArray[0];
+			return weather.description;
+		}
+	}
+);
+const data = asyncData.data;
+const pending = asyncData.pending;
+watchEffect(
+	(): void => {
+		if(data.value != null) {
+			weatherDescription.value = data.value;
+		}
+	}
+);
+</script>
+
+<template>
+	<p v-if="pending">データ取得中…</p>
+	<section v-else>
+		<h2>{{selectedCity.name}}の天気</h2>
+		<p>{{weatherDescription}}</p>
+	</section>
+	<p>リストに<NuxtLink v-bind:to="{name: 'index'}">戻る</NuxtLink></p>
+</template>
