b32cdd6267462c4807b027680af25779d5d82314
Shinzo SAITO <architshin@websarva.com>
2023-01-29 01:56:00 +0900

    refreshを原稿に合わせて変更。

--- chap04/refresh/pages/index.vue
+++ chap04/refresh/pages/index.vue
@@ -1,40 +1,36 @@
 <script setup lang="ts">
 import type {City} from "@/interfaces";
 
-//会員情報リストをステートから取得。
+//都市情報リストをステートから取得。
 const cityList = useState<Map<number, City>>("cityList");
+//初期都市IDを大阪に設定。
 const selectedCityId = ref(1853909);
+//初期都市情報を取得。
 const selectedCityInit = cityList.value.get(selectedCityId.value) as City;
+//都市情報のテンプレート変数を用意。
 const selectedCity = ref(selectedCityInit);
 
 const asyncData = await useAsyncData(
-	async () => {
-		//アクセス先URLの基本部分の変数を用意。
+	(): Promise<any> => {
 		const weatherInfoUrl = "http://api.openweathermap.org/data/2.5/weather";
-		//クエリパラメータの元データとなるオブジェクトリテラルを用意。
 		const params:{
 			lang: string,
 			q: string,
 			appId: string
 		} =
 		{
-			//言語設定のクエリパラメータ
 			lang: "ja",
-			//都市を表すクエリパラメータ。
 			q: selectedCity.value.q,
 			//APIキーのクエリパラメータ。ここに各自の文字列を記述する!!
 			appId: "913136635cfa3182bbe18e34ffd44849"
 		}
-		//クエリパラメータを生成。
 		const queryParams = new URLSearchParams(params);
-		//実際にアクセスするURLを生成。
 		const urlFull = `${weatherInfoUrl}?${queryParams}`;
-		//URLに非同期でアクセスしてデータを取得。
-		const response = await $fetch(urlFull) as any;
+		const response = $fetch(urlFull);
 		return response;
 	},
 	{
-		transform: (data): string => {
+		transform: (data: any): string => {
 			const weatherArray = data.weather;
 			const weather = weatherArray[0];
 			return weather.description;
