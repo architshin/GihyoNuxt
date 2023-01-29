6cfea2804b2cf0614aaadea527634e51c53a78c7
Shinzo SAITO <architshin@websarva.com>
2023-01-29 19:04:03 +0900

    envプロジェクトを削除。

--- chap04/env/nuxt.config.ts
+++
@@ -1,8 +0,0 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
-export default defineNuxtConfig({
-    runtimeConfig: {
-        public: {
-            weathermapAppid: process.env.NUXT_WEATHERMAP_APPID
-        }
-    }
-})
