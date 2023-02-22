b3bf6cda85a97e41677e6bd1b984e76e30e529b4
Shinzo SAITO <architshin@websarva.com>
2023-02-22 18:52:33 +0900

    error-basicにfatalエラー発生を追加。

---
+++ chap06/error-basic/pages/errorHandlerFatal.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+const onResetButtonClick = (error: any) => {
+	error.value = null;
+}
+</script>
+
+<template>
+	<ErrorGeneratorFatal/>
+	<p>
+		<NuxtLink v-bind:to="{name: 'index'}">
+			戻る
+		</NuxtLink>
+	</p>
+</template>
--- chap06/error-basic/pages/index.vue
+++ chap06/error-basic/pages/index.vue
@@ -2,12 +2,17 @@
 	<ul>
 		<li>
 			<NuxtLink v-bind:to="{name: 'errorHandlerBasic'}">
-				エラー表示実験ページ
+				エラー表示実験
 			</NuxtLink>
 		</li>
 		<li>
 			<NuxtLink v-bind:to="{name: 'errorHandlerNavigate'}">
-				画面表示時のエラー
+				画面表示時のエラー実験
+			</NuxtLink>
+		</li>
+		<li>
+			<NuxtLink v-bind:to="{name: 'errorHandlerFatal'}">
+				致命的エラー発生実験
 			</NuxtLink>
 		</li>
 	</ul>
