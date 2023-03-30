4ad11755ce9c3f8af5ed66fd21828d84fe4e0f9e
Shinzo SAITO <architshin@websarva.com>
2023-03-30 17:39:27 +0900

    composablesプロジェクトをgihyonuxt-weathers-netlifyに移植。

--- chap09/gihyonuxt-weathers-netlify/nuxt.config.ts
+++ chap09/gihyonuxt-weathers-netlify/nuxt.config.ts
@@ -1,4 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-
+	runtimeConfig: {
+		public: {
+			weatherInfoUrl: "http://api.openweathermap.org/data/2.5/weather",
+			weathermapAppid: "xxxxxx"
+		}
+	}
 })
