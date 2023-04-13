df46d59a8770be6127a17ef4992de6c48ab80d17
Shinzo SAITO <architshin@websarva.com>
2023-04-14 04:33:01 +0900

    server-storageのコードを変更。

--- chap05/server-storage/server/routes/member-management/members.get.ts
+++ chap05/server-storage/server/routes/member-management/members.get.ts
@@ -7,11 +7,11 @@ export default defineEventHandler(
 		//ストレージの用意。
 		const storage = useStorage();
 		//ストレージから会員リスト情報JSONオブジェクトを取得。
-		const memberListStorage = await storage.getItem("member-management:members");
+		const memberListStorage = await storage.getItem("local:member-management_members");
 		//会員リスト情報JSONオブジェクトが存在するなら…
 		if(memberListStorage != undefined) {
 			//会員リスト情報JSONオブジェクトを会員リストMapに変換。
-			memberList = new Map<number, Member>(memberListStorage);
+			memberList = new Map<number, Member>(memberListStorage as any);
 		}
 		const memberListValues =  memberList.values();
 		const memberListArray = Array.from(memberListValues);
--- chap05/server-storage/server/routes/member-management/members/[id].get.ts
+++ chap05/server-storage/server/routes/member-management/members/[id].get.ts
@@ -5,11 +5,11 @@ export default defineEventHandler(
 		const params = event.context.params;
 		let memberList = new Map<number, Member>();
 		const storage = useStorage();
-		const memberListStorage = await storage.getItem("member-management:members");
+		const memberListStorage = await storage.getItem("local:member-management_members");
 		if(memberListStorage != undefined) {
-			memberList = new Map<number, Member>(memberListStorage);
+			memberList = new Map<number, Member>(memberListStorage as any);
 		}
-		const idNo = Number(params.id);
+		const idNo = Number(params!.id);
 		const member = memberList.get(idNo) as Member;
 		return {
 			result: 1,
